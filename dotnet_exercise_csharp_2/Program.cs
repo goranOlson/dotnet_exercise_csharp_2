d2ee343fcea7af1922032b435956802639c9d50a
Göran Olson <goran@famolson.se>
2026-05-22 14:45:30 +0200

    Added calculate ticket price function

--- dotnet_exercise_csharp_2/Program.cs
+++ dotnet_exercise_csharp_2/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace dotnet_exercise_csharp_2
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace dotnet_exercise_csharp_2
 {
     internal class Program
     {
@@ -13,7 +16,7 @@
                 // Print main menu
                 Console.WriteLine("         Huvudmeny         ");
                 Console.WriteLine("===========================");
-                Console.WriteLine("1. Val 1");
+                Console.WriteLine("1. Beräkna kundens pris");
                 Console.WriteLine("0. Avsluta programmet");
                 Console.Write($"{Environment.NewLine}Ange siffra för ditt val: ");
 
@@ -25,7 +28,7 @@
                 }
 
                 // Handle main menu selection
-                Console.Clear();
+               Console.Clear();
                 switch (menuSelection)
                 {
                     case 0:
@@ -33,14 +36,66 @@
                         Console.WriteLine("Avslutar programmet...");
                         break;
                     case 1:
-                        Console.WriteLine("Du valde alternativ 1");
+                        CalculateTicketCost();
+                        Console.Clear();
                         break;
                     default:
                         Console.Clear();
                         Console.WriteLine($"+--------------+{Environment.NewLine}| Ogiltigt val |{Environment.NewLine}+--------------+{Environment.NewLine}");
                         break;
                 }
-            } while (keepRunning);  // (menuSelection != 0);
+            } while (keepRunning);
+
+
+            
+        }
+
+        public static void CalculateTicketCost()
+        {
+            string inputYears = "";
+            int years = -1;
+            bool validInput = false;
+
+            Console.WriteLine("  Hitta pris  ");
+            Console.WriteLine("--------------");
+
+            do
+            {
+                // Print error message
+                if (inputYears != "")
+                {
+                    Console.WriteLine($"+---------------+{Environment.NewLine}| Ogiltig ålder |{Environment.NewLine}+---------------+{Environment.NewLine}");
+                }
+
+                // Ask for customers age and calculate price
+                Console.Write("Ange kundens ålder: ");
+                inputYears = Console.ReadLine() ?? "";
+                
+                if (int.TryParse(inputYears, out years))
+                {
+                    if (years < 20)
+                    {
+                        Console.WriteLine("Ungdomspris: 80 kr");
+                    }
+                    else if (years > 64)
+                    {
+                        Console.WriteLine("Pensionärspris: 90 kr");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Standartpris: 120 kr");
+                    }
+
+                    // Exit loop and return to main menu
+                    Console.WriteLine($"{Environment.NewLine}Tryck på valfri tangent för att gå till huvudmenyn");
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    if (key.ToString() != "")
+                    {
+                        validInput = true;
+                    }
+                }
+            } while (!validInput);
+            // Console.Clear();
         }
     }
 }
