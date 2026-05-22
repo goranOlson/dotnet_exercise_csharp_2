2364ecfdad32db8e9c33d66fd05187086ada2a20
Göran Olson <goran@famolson.se>
2026-05-22 13:21:21 +0200

    Created basic main menu.

--- dotnet_exercise_csharp_2/Program.cs
+++ dotnet_exercise_csharp_2/Program.cs
@@ -5,6 +5,41 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            string input = "";
+            int menuSelection = -1;
+
+            do
+            {
+                // Print main menu
+                Console.WriteLine($"{Environment.NewLine}___Huvudmeny___");
+                Console.WriteLine("1. Val 1");
+                Console.WriteLine("0. Avsluta programmet");
+                Console.Write($"{Environment.NewLine}Ditt val: ");
+                input = Console.ReadLine() ?? "";
+
+                // Capture user input and validate it
+                if (!int.TryParse(input, out menuSelection))
+                {
+                    menuSelection = -1;
+                }
+
+                // Handle main menu selection
+                Console.Clear();
+                switch (menuSelection)
+                {
+                    case 0:
+                        Console.WriteLine("Avslutar programmet...");
+                        break;
+                    case 1:
+                        Console.WriteLine("Du valde alternativ 1");
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine($"+--------------+{Environment.NewLine}| Ogiltigt val |{Environment.NewLine}+--------------+");
+                        break;
+                }
+            } while (menuSelection != 0);
         }
     }
 }
