5d5049991506dbc923cd93f4b55a4e10c4325df0
Göran Olson <goran@famolson.se>
2026-05-22 16:03:51 +0200

    [WIP] Working on group price for tickets

--- dotnet_exercise_csharp_2/Program.cs
+++ dotnet_exercise_csharp_2/Program.cs
@@ -16,11 +16,13 @@ namespace dotnet_exercise_csharp_2
                 // Print main menu
                 Console.WriteLine("         Huvudmeny         ");
                 Console.WriteLine("===========================");
-                Console.WriteLine("1. Beräkna kundens pris");
+                Console.WriteLine("1. Beräkna pris för kund");
+                Console.WriteLine("2. Beräkna pris för grupp");
                 Console.WriteLine("0. Avsluta programmet");
                 Console.Write($"{Environment.NewLine}Ange siffra för ditt val: ");
 
                 // Capture user input and validate it
+
                 input = Console.ReadLine() ?? "";
                 if (!int.TryParse(input, out menuSelection))
                 {
@@ -28,7 +30,7 @@ namespace dotnet_exercise_csharp_2
                 }
 
                 // Handle main menu selection
-               Console.Clear();
+                Console.Clear();
                 switch (menuSelection)
                 {
                     case 0:
@@ -39,15 +41,69 @@ namespace dotnet_exercise_csharp_2
                         CalculateTicketCost();
                         Console.Clear();
                         break;
+                    case 2:
+                        Console.WriteLine("Alternativ 2");
+                        CalculateGroupPrice();
+                        //Console.Clear();
+                        break;
                     default:
                         Console.Clear();
                         Console.WriteLine($"+--------------+{Environment.NewLine}| Ogiltigt val |{Environment.NewLine}+--------------+{Environment.NewLine}");
                         break;
                 }
+                Console.Clear();
+                // Console.WriteLine("Next loop");
             } while (keepRunning);
+        }
 
-
+        public static void CalculateGroupPrice()
+        {
+            int sum = 0;
+            int count = 0;
+            int years = -1;
+            string input = "";
             
+            Console.WriteLine("  Beräkna pris för grupp  ");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine($"Avbryt med tom rad{Environment.NewLine}");
+
+
+            do
+            {
+                Console.Write("Ange ålder: ");
+                input = Console.ReadLine() ?? "";
+                if (int.TryParse(input, out years) && years > 0)
+                {
+                    // Console.WriteLine("Good years");
+
+                    count++;
+                    switch (years)
+                    {
+                        case < 20:
+                            sum += 80;
+                            break;
+                        case > 64:
+                            sum += 90;
+                            break;
+                        default:
+                            sum += 120;
+                            break;
+                    }
+                }
+                else if (input != "")
+                {
+                    Console.WriteLine("Bad years");
+                }
+
+            } while(input != "");
+            // Console.WriteLine("Klar med grupp...");
+
+            // sum och antal
+            // Console.WriteLine($"Antal personer: {count}, summa: {sum} kr");
+            Console.WriteLine($"Pris: {sum} för {count} personer");
+
+            Stopper();
+            // Console.Clear();
         }
 
         public static void CalculateTicketCost()
@@ -56,8 +112,8 @@ namespace dotnet_exercise_csharp_2
             int years = -1;
             bool validInput = false;
 
-            Console.WriteLine("  Hitta pris  ");
-            Console.WriteLine("--------------");
+            Console.WriteLine("  Beräkna pris för enskild kund  ");
+            Console.WriteLine("-------------------------");
 
             do
             {
@@ -70,7 +126,7 @@ namespace dotnet_exercise_csharp_2
                 // Ask for customers age and calculate price
                 Console.Write("Ange kundens ålder: ");
                 inputYears = Console.ReadLine() ?? "";
-                
+
                 if (int.TryParse(inputYears, out years))
                 {
                     if (years < 20)
@@ -87,15 +143,29 @@ namespace dotnet_exercise_csharp_2
                     }
 
                     // Exit loop and return to main menu
-                    Console.WriteLine($"{Environment.NewLine}Tryck på valfri tangent för att gå till huvudmenyn");
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    if (key.ToString() != "")
-                    {
-                        validInput = true;
-                    }
+                    //Console.WriteLine($"{Environment.NewLine}Tryck på valfri tangent för att gå till huvudmenyn");
+                    //ConsoleKeyInfo key = Console.ReadKey();
+                    //if (key.ToString() != "")
+                    //{
+                    //    validInput = true;
+                    //}
+                    validInput = Stopper();
                 }
             } while (!validInput);
             // Console.Clear();
+        }
+
+        public static bool Stopper()
+        {
+            bool validInput = false;
+            Console.WriteLine($"{Environment.NewLine}Tryck på valfri tangent för att gå till huvudmenyn");
+            ConsoleKeyInfo key = Console.ReadKey();
+            if (key.ToString() != "")
+            {
+                validInput = true;
+            }
+
+            return validInput;
         }
     }
 }
